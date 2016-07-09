dada5f050e39f966e45908f53b8a33a80336d461
ydeepalireddy <ydeepalireddy@gmail.com>
2016-07-09 20:43:20 +0530

    with health and academics

--- samridhi_site/forms.py
+++ samridhi_site/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from .models import Baseline
 from .models import Enrollments
+from .models import Health
+from .models import Academics
+
 
 class BaselineForm(forms.ModelForm):
      class Meta:
@@ -12,3 +15,14 @@ class EnrollmentForm(forms.ModelForm):
        class Meta:
        model=Enrollments
        fields=('id','name','fname','native_lang','survey_taken','siblings','photo','age','date')
+
+class AcademicsForms(forms.ModelForms):
+       class Meta:
+            model=Academics
+            fields=('id','name','date','math','science','english','hindi','extracuricular_activities')
+            
+            
+class HealthForms(forms.ModelForms):
+       class Meta:
+           model=Health
+           fields=('id','name','date','student_health','fathers_health','mothers_health','siblings_health')
