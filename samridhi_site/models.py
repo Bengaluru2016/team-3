f19d001c47a62b3769616348dd163a9e3d65edd3
farhaanbukhsh <farhaan.bukhsh@gmail.com>
2016-07-09 18:11:04 +0530

    Add models

--- samridhi_site/models.py
+++ samridhi_site/models.py
@@ -5,7 +5,28 @@ class Baseline(models.Model):
     """ Survey Data"""
 
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200)
-    fathersname = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, default='SOME STRING')
+    fname = models.CharField(max_length=200, default='SOME STRING')
+    native_lang = models.CharField(max_length=200, default='SOME STRING')
+    siblings = models.IntegerField(default=0)
+    photo = models.BooleanField(default=False)
+    age = models.IntegerField(default=False)
+    date = models.DateTimeField(
+                        default=timezone.now)
+
+
+class Enrollment(models.Model):
+    """ Enrolled data"""
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, default='SOME STRING')
+    fname = models.CharField(max_length=200, default='SOME STRING')
+    native_lang = models.CharField(max_length=200, default='SOME STRING')
+    survey_taken = models.BooleanField(default=False)
+    siblings = models.IntegerField(default=0)
+    photo = models.BooleanField(default=False)
+    age = models.IntegerField(default=False)
+    date = models.DateTimeField(
+                        default=timezone.now)
 
 # Create your models here.
